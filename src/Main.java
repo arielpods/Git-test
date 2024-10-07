4ca02a8f47924a3019b83cc60dcb68200dbfa8e6
Ariela AGBA <ariela.agba@edu.esiee-it.fr>
2024-10-07 13:28:29 +0200

    Add log

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcomea");
+        System.out.printf("Hello Ariela");
 
     }
 }
